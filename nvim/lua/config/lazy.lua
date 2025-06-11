87f42d117d326eabef5cdd058f34c6ffce60bada
Pirateness <39227245+Pirateness@users.noreply.github.com>
2025-06-11 20:01:30 +1000

    added more plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,23 +23,23 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = false },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = false },
 })
 
 -- Blink.cmp Setup
 require("blink.cmp").setup({
-	keymap = { preset = 'super-tab' },
+	keymap = { preset = "super-tab" },
 	sources = {
 		default = { "lsp", "path", "snippets", "buffer" },
-		providers = {}
+		providers = {},
 	},
 	completion = {
 		keyword = { range = "full" },
@@ -48,52 +48,58 @@ require("blink.cmp").setup({
 		},
 		ghost_text = { enabled = true },
 		documentation = {
-		  auto_show = true,
-		  auto_show_delay_ms = 500,
-		  window = {
-		    border = "rounded",
-		    winblend = 0,
-		  },
+			auto_show = true,
+			auto_show_delay_ms = 500,
+			window = {
+				border = "rounded",
+				winblend = 0,
+			},
 		},
 		menu = {
-		  draw = { treesitter = { "lsp" } },
-		  border = "rounded",
-		  winblend = 0,
+			draw = { treesitter = { "lsp" } },
+			border = "rounded",
+			winblend = 0,
 		},
-	      },
-	      signature = {
+	},
+	signature = {
 		enabled = true,
 		window = {
-		  border = "rounded",
-		  winblend = 0,
+			border = "rounded",
+			winblend = 0,
 		},
-	}
+	},
 })
 
 -- LSP & Autocomplete Setup
--- When adding a new language server, first add it via Mason the enable it via
--- LSPConfig below, as show in the "ts_ls" example
 require("mason").setup()
 require("mason-lspconfig").setup()
 
 -- UFO Config
 --
-vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldcolumn = "0" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
--- 
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+--
 -- UFO Config
 
 -- Tag Auto-Close Setup
-require('nvim-ts-autotag').setup()
+require("nvim-ts-autotag").setup()
 
 -- Character Auto-Close Setup
 require("nvim-autopairs").setup()
+
+-- Treesitter context setup
+require("treesitter-context").setup({
+	enable = true,
+})
+
+-- Hardtime Setup
+--require("hardtime").setup()
 
 -- Snippet Provider
 -- require("luasnip.loaders.from_vscode").lazy_load()
@@ -102,57 +108,79 @@ require("nvim-autopairs").setup()
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- Setup Theme Manager
+require("themery").setup({
+	themes = { "catppuccin", "kanagawa", "nightfox", "oxocarbon", "gruvbox" }, -- Your list of installed colorschemes.
+	livePreview = true, -- Apply theme while picking. Default to true.
+})
+
+-- Setup Aerial
+require("aerial").setup({
+	layout = {
+		max_width = { 40, 0.4 },
+		min_width = 30,
+	},
+	-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+	on_attach = function(bufnr)
+		-- Jump forwards/backwards with '{' and '}'
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
+})
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+
 -- Change colorscheme
 require("catppuccin").setup({
 	integrations = {
 		blink_cmp = true,
 		treesitter = true,
-	}
+	},
 })
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
 -- Setup Lualine (bottom status bar)
-require('lualine').setup()
+require("lualine").setup()
 
 -- Setup Format on Save
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-  callback = function(args)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = args.buf,
-      callback = function()
-        vim.lsp.buf.format {async = false, id = args.data.client_id }
-      end,
-    })
-  end
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
+-- Customise Tab Sizing
+local tabSize = 4
+vim.opt.shiftwidth = tabSize
+vim.opt.tabstop = tabSize
+vim.opt.softtabstop = tabSize
+
 -- Make neovim background transparent
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = 'none' })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
 -- Additional Transparency
 vim.diagnostic.config({
-  float = {
-    border = "rounded", -- or "single", "double", "shadow", etc.
-  },
+	float = {
+		border = "rounded", -- or "single", "double", "shadow", etc.
+	},
 })
 
 -- Telescope Keybinds
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 -- GENERAL KEYBINDS --
 
 -- Show diagnostics for the current line
 vim.keymap.set("n", "<leader>d", function()
-  vim.diagnostic.open_float(nil, { focusable = false, scope = "line" })
+	vim.diagnostic.open_float(nil, { focusable = false, scope = "line" })
 end, { desc = "Show line diagnostics" })
 
 -- Go to Definition LSP Override
---
+++ nvim/lua/plugins/themery.lua
@@ -0,0 +1,8 @@
+return {
+	"rebelot/kanagawa.nvim",
+	"EdenEast/nightfox.nvim",
+	"nyoom-engineering/oxocarbon.nvim",
+	"zaldih/themery.nvim",
+	"morhetz/gruvbox",
+	lazy = false,
+}
